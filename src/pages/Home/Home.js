149e2ad7adf082dc56446899d6ba4d7e304a00b3
Sebastián Rojas Vargas <sebasrv26@gmail.com>
2022-03-14 14:07:27 -0600

    Structure

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+//import classes from "./Home.module.css";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Works!</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+//import classes from "./Login.module.css";
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login Works!</h1>
+        </div>
+    );
+};
+
+export default Login;
